0661ce51c20887249c1905f033fb097dbb3b8a04
adamcharusta <dQa!n26H4u>
2023-03-19 00:49:07 +0100

    add card to search panel, change body bgc

--- src/components/SearchGeolocationComponent/SearchGeolocationComponent.tsx
+++ src/components/SearchGeolocationComponent/SearchGeolocationComponent.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useContext, useState } from 'react';
-import { Autocomplete, CircularProgress, TextField } from '@mui/material';
+import { Autocomplete, Card, CardContent, CircularProgress, TextField } from '@mui/material';
 import { GeolocationResponseType, GeolocationType } from '../../types/geolocationTypes';
 import { geolocationApiInstance } from '../../apiInstances';
 import { AxiosResponse } from 'axios';
@@ -33,36 +33,40 @@ const SearchGeolocationComponent = () => {
 
   // TODO: Fix "MUI: The value provided to Autocomplete is invalid." warn.
   return (
-    <Autocomplete
-      disablePortal
-      id='geolocation-search'
-      options={options}
-      sx={{ width: '100%', marginTop: 1 }}
-      loading={loading}
-      value={geolocation}
-      getOptionLabel={(option) => option.name}
-      isOptionEqualToValue={(option, value) => option.id === value.id}
-      onChange={handleAutocompleteChange}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label='Location'
-          onChange={handleInputChange}
-          InputProps={{
-            ...params.InputProps,
-            endAdornment: (
-              <React.Fragment>
-                {loading ? <CircularProgress color='inherit' size={20} /> : null}
-                {params.InputProps.endAdornment}
-              </React.Fragment>
-            ),
-          }}
+    <Card>
+      <CardContent>
+        <Autocomplete
+          disablePortal
+          id='geolocation-search'
+          options={options}
+          sx={{ width: '100%', marginTop: 1, backgroundColor: '#fff' }}
+          loading={loading}
+          value={geolocation}
+          getOptionLabel={(option) => option.name}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          onChange={handleAutocompleteChange}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label='Location'
+              onChange={handleInputChange}
+              InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <React.Fragment>
+                    {loading ? <CircularProgress color='inherit' size={20} /> : null}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}
+            />
+          )}
+          renderOption={(params, option) => (
+            <GeolocationOptionComponent key={option.id} params={params} geolocation={option} />
+          )}
         />
-      )}
-      renderOption={(params, option) => (
-        <GeolocationOptionComponent key={option.id} params={params} geolocation={option} />
-      )}
-    />
+      </CardContent>
+    </Card>
   );
 };
 
