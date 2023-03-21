b10617b0bb428a42eb928ca3eefa2f6d70213681
adamcharusta <dQa!n26H4u>
2023-03-21 14:09:21 +0100

    fix enum

--- src/components/WeatherPanelComponent/WeeklyWeatherCardComponent.tsx
+++ src/components/WeatherPanelComponent/WeeklyWeatherCardComponent.tsx
@@ -3,7 +3,7 @@ import AppContext from '../../appContext';
 import { Card, CardContent, Typography } from '@mui/material';
 import ChartComponent from '../ChartComponent/ChartComponent';
 
-const enum EXPANDED_CHARTS {
+enum EXPANDED_CHARTS_ENUM {
   temperature2m,
   windspeed10m,
   relativehumidity2m,
@@ -13,7 +13,9 @@ const enum EXPANDED_CHARTS {
 }
 
 const WeeklyWeatherCardComponent = () => {
-  const [expanded, setExpanded] = useState<EXPANDED_CHARTS>(EXPANDED_CHARTS.temperature2m);
+  const [expanded, setExpanded] = useState<EXPANDED_CHARTS_ENUM>(
+    EXPANDED_CHARTS_ENUM.temperature2m,
+  );
   const { weather } = useContext(AppContext);
 
   if (!weather) return <></>;
@@ -27,48 +29,48 @@ const WeeklyWeatherCardComponent = () => {
           data={weather?.hourly.temperature_2m}
           unit={weather?.hourly_units.temperature_2m}
           title='Temperature'
-          expanded={expanded === EXPANDED_CHARTS.temperature2m}
-          setExpanded={() => setExpanded(EXPANDED_CHARTS.temperature2m)}
+          expanded={expanded === EXPANDED_CHARTS_ENUM.temperature2m}
+          setExpanded={() => setExpanded(EXPANDED_CHARTS_ENUM.temperature2m)}
         />
         <ChartComponent
           hours={weather?.hourly.time}
           data={weather?.hourly.windspeed_10m}
           title='Wind speed'
           unit={weather?.hourly_units.windspeed_10m}
-          expanded={expanded === EXPANDED_CHARTS.windspeed10m}
-          setExpanded={() => setExpanded(EXPANDED_CHARTS.windspeed10m)}
+          expanded={expanded === EXPANDED_CHARTS_ENUM.windspeed10m}
+          setExpanded={() => setExpanded(EXPANDED_CHARTS_ENUM.windspeed10m)}
         />
         <ChartComponent
           hours={weather?.hourly.time}
           data={weather?.hourly.relativehumidity_2m}
           title='Relative humidity'
           unit={weather?.hourly_units.relativehumidity_2m}
-          expanded={expanded === EXPANDED_CHARTS.relativehumidity2m}
-          setExpanded={() => setExpanded(EXPANDED_CHARTS.relativehumidity2m)}
+          expanded={expanded === EXPANDED_CHARTS_ENUM.relativehumidity2m}
+          setExpanded={() => setExpanded(EXPANDED_CHARTS_ENUM.relativehumidity2m)}
         />
         <ChartComponent
           hours={weather?.hourly.time}
           data={weather?.hourly.rain}
           title='Rain'
           unit={weather?.hourly_units.rain}
-          expanded={expanded === EXPANDED_CHARTS.rain}
-          setExpanded={() => setExpanded(EXPANDED_CHARTS.rain)}
+          expanded={expanded === EXPANDED_CHARTS_ENUM.rain}
+          setExpanded={() => setExpanded(EXPANDED_CHARTS_ENUM.rain)}
         />
         <ChartComponent
           hours={weather?.hourly.time}
           data={weather?.hourly.snowfall}
           title='Snow fall'
           unit={weather?.hourly_units.snowfall}
-          expanded={expanded === EXPANDED_CHARTS.snowfall}
-          setExpanded={() => setExpanded(EXPANDED_CHARTS.snowfall)}
+          expanded={expanded === EXPANDED_CHARTS_ENUM.snowfall}
+          setExpanded={() => setExpanded(EXPANDED_CHARTS_ENUM.snowfall)}
         />
         <ChartComponent
           hours={weather?.hourly.time}
           data={weather?.hourly.visibility}
           title='Visibility'
           unit={weather?.hourly_units.visibility}
-          expanded={expanded === EXPANDED_CHARTS.visibility}
-          setExpanded={() => setExpanded(EXPANDED_CHARTS.visibility)}
+          expanded={expanded === EXPANDED_CHARTS_ENUM.visibility}
+          setExpanded={() => setExpanded(EXPANDED_CHARTS_ENUM.visibility)}
         />
       </CardContent>
     </Card>
